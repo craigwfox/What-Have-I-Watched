98c38520f5f70f0a712fa46f4decc80426fb4ccb
Craig Fox <craig@craigwfox.com>
2023-07-06 20:21:53 -0500

    css stripped out

---
+++ src/lib/components/MovieCard.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	// imports
+	import MovieLink from '$lib/components/MovieLink.svelte';
+
+	// exports
+	export let movie: object;
+</script>
+
+<article class="movie-card" aria-labelledby={movie.id}>
+	{#if movie.backdrop_path}
+		<img
+			src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+			alt={`Movie poster for ${movie.name}`}
+			loading="lazy"
+		/>
+	{/if}
+	<MovieLink movieData={movie}>
+		<h2 id={movie.id}>{movie.name}</h2>
+		<ul>
+			<li>Watch date: {movie.watch_date}</li>
+			{#if movie.rating_craig}
+				{#if movie.rating_rebecca == null}
+					<li>Rating: {movie.rating_craig}</li>
+				{:else}
+					<li>Craig Rating: {movie.rating_craig}</li>
+					<li>Rebecca Rating: {movie.rating_rebecca}</li>
+				{/if}
+			{/if}
+			{#if movie.picked}
+				<li>Picked by: {movie.picked}</li>
+			{/if}
+		</ul>
+	</MovieLink>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import MovieLink from '$lib/components/MovieLink.svelte';
 	import { object_without_properties } from 'svelte/internal';
+	import MovieCard from '$lib/components/MovieCard.svelte';
 
 	export let data;
 	let { movies } = data;
@@ -19,32 +19,6 @@
 
 <div class="grid">
 	{#each movies as movie}
-		<article class="movie-card" aria-labelledby={movie.id}>
-			{#if movie.backdrop_path}
-				<img
-					src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
-					alt={`Movie poster for ${movie.name}`}
-					loading="lazy"
-				/>
-			{/if}
-			<MovieLink movieData={movie}>
-				<h2 id={movie.id}>{movie.name}</h2>
-				<ul>
-					<li>Watch date: {movie.watch_date}</li>
-					<li>IMDB ID: {movie.imdb_id}</li>
-					{#if movie.rating_craig}
-						{#if movie.rating_rebecca == null}
-							<li>Rating: {movie.rating_craig}</li>
-						{:else}
-							<li>Craig Rating: {movie.rating_craig}</li>
-							<li>Rebecca Rating: {movie.rating_rebecca}</li>
-						{/if}
-					{/if}
-					{#if movie.picked}
-						<li>Picked by: {movie.picked}</li>
-					{/if}
-				</ul>
-			</MovieLink>
-		</article>
+		<MovieCard {movie} />
 	{/each}
 </div>
